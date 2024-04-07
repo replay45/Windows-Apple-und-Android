b7770cc730b054ed9b2154660cba5ec36afce038
replay45 <143552987+replay45@users.noreply.github.com>
2024-04-07 14:27:05 +0200

    Delete matrix-bat-windows/matrix-random.bat

--- matrix-bat-windows/matrix-random.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-color 0a
-mode 1000
-:a
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
-goto a
